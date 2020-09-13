c3036eb5991c538710a549873b9727951f7c2dae
Anders Birkenes <datagutten@datagutten.net>
2020-09-13 21:28:40 +0200

    Correct app name

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'employee_info.apps.EmployeeInfoConfig',
+    'ad_import.apps.AdImportConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
