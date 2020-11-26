3ae94d32fa4ee83bfa2cd572829101dfff2ee9a7
datagutten <datagutten@datagutten.net>
2020-11-26 15:44:26 +0100

    Add directory name fields and lookup method

--- ad_import/models/directory.py
+++ ad_import/models/directory.py
@@ -1,8 +1,14 @@
+from __future__ import annotations
+
 from django.db import models
 
 
 class Directory(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
+    dns_name = models.CharField('Domain name',
+                                max_length=100, null=True, blank=True)
+    short_name = models.CharField('Domain name (pre-Windows 2000)',
+                                  max_length=100, null=True, blank=True)
     dn = models.CharField(max_length=100)
     dc = models.GenericIPAddressField()
     username = models.CharField(max_length=100)
@@ -15,3 +21,10 @@ class Directory(models.Model):
     class Meta:
         # verbose_name = 'Directory'
         verbose_name_plural = 'directories'
+
+    @staticmethod
+    def find_directory(name: str) -> Directory:
+        try:
+            return Directory.objects.get(dns_name__iexact=name)
+        except Directory.DoesNotExist:
+            return Directory.objects.get(short_name__iexact=name)
