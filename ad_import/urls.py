3c6a8593db7da6b9e03c73449ab85393484882c7
datagutten <datagutten@datagutten.net>
2022-03-03 10:45:55 +0100

    Add views for workstations and servers

--- ad_import/urls.py
+++ ad_import/urls.py
@@ -5,5 +5,7 @@ from ad_import import views
 app_name = 'ad_import'
 
 urlpatterns = [
-    path('user', views.view_user, name='user')
+    path('user', views.view_user, name='user'),
+    path('computer', views.view_workstation, name='workstation'),
+    path('server', views.view_server, name='server'),
 ]
